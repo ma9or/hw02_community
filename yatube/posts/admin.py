f7beae11684de1e6dcbacc4cc052d3f31e316d52
Ильдар Ахметжанов <il.akhmetzhanov@yandex.kz>
2021-12-20 22:17:38 +0600

    Final project sprint3

---
+++ yatube/posts/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+# Register your models here.
+# Из модуля models импортируем модель Post
+from .models import Post, Group
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+
+# класс PostAdmin
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    ) 
+    list_editable = ('group',)
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',) 
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',) 
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строк
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    seach_fields = ('title',)
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
---
+++ yatube/posts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+# Импортируем модель, чтобы обратиться к ней
+# from .models import Post
+
+
+# Главная страница
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': 'Последние обновления на сайте',
+        'posts': posts,
+    }
+    # Третьим параметром передаём словарь context
+    
+    return render(request, 'posts/index.html', context) 
+
+
+# Страница со списком постов
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данны{% block title %}
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': 'записи сообщества',
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
